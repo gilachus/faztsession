57166b25eaa1b395d113b42884a3ff72d821fbd1
jesusgil <jgilb@unicartagena.edu.co>
2019-11-08 05:05:46 +0000

    fin de la practica de sessions

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const session = require('express-session');
+var flash = require('connect-flash'); 
+
 
 //settings
 app.set('views', path.join(__dirname,'views'));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 //middleware
-
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(session({
+    secret: 'mysecretkey',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(flash());
+//global variable
+app.use((req,res,next)=>{
+    app.locals.message = req.flash('success');
+    next();
+});
 //routes
 app.use(require('./routes/routes'));
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,35 @@
 const { Router } = require('express');
 const router = Router();
 
-router.get('/', (req, res) => res.send('Habla cachon this is EXPRESS!'))
+
+//router.get('/', (req, res) => res.send('Habla cachon this is EXPRESS!'));
+router.get('/', (req, res) => {
+     res.render('index.ejs');
+});
+router.post('/register', (req, res) => {
+     console.log(req.body);
+     //req.session.my_variable = 'hello';
+     //res.send('received');
+
+     //req.session.my_variable = req.body;
+
+     req.flash('usuario', req.body);
+     req.flash('success','estas registrado');
+     res.redirect('/products');
+});
+router.get('/profile', (req, res) => {
+     //const data = req.session.my_variable;
+     //delete req.session.my_variable;
+
+     const data = req.flash('usuario')[0];
+     res.render('profile', {data});
+});
+router.get('/products', (req, res) => {
+     // const data = req.flash('usuario')[0];
+     // const message = req.flash('success')[0];
+     // res.render('profile', {data, message});
+     res.render('products');
+});
+
+
 module.exports = router;
